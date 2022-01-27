76e565a3e37c08b757214710835bb30830d83019
bulakh <bulakh94@gmail.com>
2022-01-27 16:28:43 +0300

    feat: add filter characters

--- src/pages/Characters/Characters.tsx
+++ src/pages/Characters/Characters.tsx
@@ -1,29 +1,43 @@
-import React, {FC, useEffect, useState} from "react";
+import React, {ChangeEvent, FC, useEffect, useState} from "react";
 import CharacterItem from "../../components/CharacterItem";
 import List from "../../components/UI/List";
 import { fetchCharacters } from "../../hooks/useFetch";
+import { findCharacter } from "../../hooks/useFilter";
 import { ICharacter } from "../../types/types";
 
 
 
 const Characters: FC = () => {
   const [characters, setCharacters] = useState<ICharacter[]>([]);
+  const [find, setFind] = useState<string>('');
   
   useEffect(() => {
     fetchCharacters(setCharacters);
   }, []);
 
+ 
+
+  const changeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setFind(e.target.value);
+  };
+
+  const filteredCharacters = findCharacter(find, characters);
+  
+
   return (
     <>
       <h1>Characters</h1>
 
+      <input onChange={changeInputHandler} type="text" value={find} placeholder="Search character..."/>
+      {/* <Input onChange={(e: ChangeEvent<HTMLInputElement>) => changeInputHandler} value={find} placeholder="Search character..." /> */}
+
       <List 
         flex
-        items={characters} 
+        items={find === '' ? characters : filteredCharacters} 
         renderItem={(character: ICharacter) => <CharacterItem character={character} key={character.char_id} />}
       />
     </>
   );
-}
+};
 
 export default Characters;
