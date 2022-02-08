7ad49d3e00ad4d53c36ac77a37919faed1237aeb
bulakh <bulakh94@gmail.com>
2022-02-08 17:02:01 +0300

    feat: add filter nickname and animation for character card

--- src/components/CharacterItem.tsx
+++ src/components/CharacterItem.tsx
@@ -1,23 +1,80 @@
 import React, {FC} from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { Fonts } from "../styles/variables";
 import { ICharacter } from "../types/types";
 
 interface CharacterItemProps {
     character: ICharacter;
-}
+};
+
+interface Props {
+  src: string;
+};
+
+const Animation = css`
+  position: relative;
+  >.front,
+  >.back {
+      display: block;
+      transition-timing-function: cubic-bezier(.175, .885, .32, 1.275);
+      transition-duration: 0.5s;
+      transition-property: transform, opacity;
+  }
+  >.front {
+      transform: rotateY(0deg);
+  }
+  >.back {
+      position: absolute;
+      opacity: 0;
+      top: 0px;
+      left: 0px;
+      width: 100%;
+      height: 100%;
+      transform: rotateY(-180deg);
+  }
+  >.front,
+  >.back {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    color: white;
+    background-size: cover;
+    background-position: center;
+    background-color: #313131;
+    border-radius: 3px;
+    text-align: center;
+    p {
+      font-family: ${Fonts.cooper}
+      font-size: 25px;
+    }
+  }
+  &:hover {
+      >.front {
+          transform: rotateY(180deg);
+      }
+      >.back {
+          opacity: 1;
+          transform: rotateY(0deg);
+      }
+  }
+`
 
 const StyledItem = styled.li`
+  ${Animation};
+
   width: 214px;
   height: 315px;
 
   cursor: pointer;
 `
 
-const StyledImg = styled.img`
+const ImageWrap = styled.div<Props>`
   width: 100%;
   height: 100%;
   object-fit: cover;
+  background-image: ${({src}) => `url(${src})`}
 `
 
 const CharacterItem: FC<CharacterItemProps> = ({character}) => {
@@ -29,9 +86,11 @@ const CharacterItem: FC<CharacterItemProps> = ({character}) => {
 
   return (
     <StyledItem onClick={clickHandler}>
-      <StyledImg src={character.img} alt={character.name} />
+      <ImageWrap className="front" src={character.img} />
+      <div className="back">
+        <p>{character.name}</p>
+      </div>
 
-      {/* <p>{character.char_id}. {character.name}</p> */}
     </StyledItem>
 
   );
--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,9 +1,8 @@
-interface Filterwise {
-    name: string,
-}
+import { ICharacter } from "../types/types";
 
-export const findCharacter = <T extends Filterwise>(find: string, persons:T[]):T[] => {
+
+export const findCharacter = <T extends ICharacter>(find: string, persons:T[]):T[] => {
     const regex = new RegExp(find, 'gi')
-    const filteredPersons = persons.filter(person => person.name.match(regex));
+    const filteredPersons = persons.filter(person => (person.name.match(regex) || person.nickname.match(regex)));
     return filteredPersons;
 };
