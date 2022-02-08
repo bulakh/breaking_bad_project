bdc5df671d99379fd1b187b701d427a461e661d7
bulakh <bulakh94@gmail.com>
2022-02-08 14:37:37 +0300

    feat: add styles for deaths page

--- src/components/DeathItem.tsx
+++ src/components/DeathItem.tsx
@@ -1,4 +1,5 @@
 import React, {FC} from "react";
+import styled from "styled-components";
 import { IDeath } from "../types/types";
 
 
@@ -6,14 +7,32 @@ interface DeathItemProps {
     death: IDeath;
 }
 
+const StyledItems = styled.li`
+  margin-bottom: 40px;
+`
+
+const DeathTitle = styled.h2`
+  font-size: 35px;
+  margin-bottom: 20px;
+`
+
+const DeathText = styled.p`
+  margin-bottom: 15px;
+  font-size: 20px;
+  font-weight: 300;
+`
+
+
+
 const DeathItem: FC<DeathItemProps> = ({death}) => {
   return (
-    <li>
-        <h3>{death.death_id}. {death.death}</h3>
-        <p>Responsible: {death.responsible}</p>
-        <p>Cause: {death.cause}</p>
-        <p>Episode: {death.episode}</p>
-    </li>
+    <StyledItems>
+        <DeathTitle>{death.death} 💀</DeathTitle>
+        <DeathText> 🪒 Responsible: {death.responsible}</DeathText>
+        <DeathText> ❌ Cause: {death.cause}</DeathText>
+        <DeathText> 💬 Last words: &#8220;{death.last_words}&#8221;</DeathText>
+        🎥  Season: {death.season} 🎬 Episode: {death.episode}
+    </StyledItems>
   );
 }
 
